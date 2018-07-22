bb0a055d0a765119af544e2c48c35d15806bacc0
danives <danives@btopenworld.com>
2018-07-23 00:43:49 +0100

    Updates and changes to get a more exact image

--- cardtest.py
+++ cardtest.py
@@ -2,6 +2,8 @@ import numpy as np
 import imutils
 import cv2
 
+from imutils import perspective
+
 BKG_THRESH = 60
 CARD_THRESH = 30
 
@@ -9,149 +11,123 @@ CARD_MAX_AREA = 120000
 CARD_MIN_AREA = 15000
 
 def find_cards(thresh_image):
-    """Finds all card-sized contours in a thresholded camera image.
-    Returns the number of cards, and a list of card contours sorted
-    from largest to smallest."""
-
-    # Find contours and sort their indices by contour size
-    dummy,cnts,hier = cv2.findContours(thresh_image,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    index_sort = sorted(range(len(cnts)), key=lambda i : cv2.contourArea(cnts[i]),reverse=True)
-
+    
+    # Find contours
+    dummy, cnts, hier = cv2.findContours(thresh_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    
     # If there are no contours, do nothing
     if len(cnts) == 0:
-        return [], []
+        return []
     
-    # Otherwise, initialize empty sorted contour and hierarchy lists
-    cnts_sort = []
-    hier_sort = []
-    cnt_is_card = np.zeros(len(cnts),dtype=int)
-
-    # Fill empty lists with sorted contour and sorted hierarchy. Now,
-    # the indices of the contour list still correspond with those of
-    # the hierarchy list. The hierarchy array can be used to check if
-    # the contours have parents or not.
-    for i in index_sort:
-        cnts_sort.append(cnts[i])
-        hier_sort.append(hier[0][i])
+    cards = []
 
     # Determine which of the contours are cards by applying the
     # following criteria: 1) Smaller area than the maximum card size,
     # 2), bigger area than the minimum card size, 3) have no parents,
     # and 4) have four corners
 
-    for i in range(len(cnts_sort)):
-        size = cv2.contourArea(cnts_sort[i])
-        peri = cv2.arcLength(cnts_sort[i],True)
-        approx = cv2.approxPolyDP(cnts_sort[i],0.01*peri,True)
+    for i in range(len(cnts)):
+        size = cv2.contourArea(cnts[i])
+        peri = cv2.arcLength(cnts[i],True)
+        approx = cv2.approxPolyDP(cnts[i],0.01*peri,True)
         
-        if ((size < CARD_MAX_AREA) and (size > CARD_MIN_AREA)
-            and (hier_sort[i][3] == -1) and (len(approx) == 4)):
-            cnt_is_card[i] = 1
+        if ((size < CARD_MAX_AREA) and (size > CARD_MIN_AREA) and (len(approx) == 4)) and (hier[0][i][3] == -1):
+            cards.append(cnts[i])
 
-    return cnts_sort, cnt_is_card
+    return cards
 
 
-def preprocess_card(contour, image):
-    # Find perimeter of card and use it to approximate corner points
+def preprocess_card(contour, image, ratio):
     peri = cv2.arcLength(contour,True)
-    approx = cv2.approxPolyDP(contour,0.01*peri,True)
-    pts = np.float32(approx)
-    corner_pts = pts
+    approx = cv2.approxPolyDP(contour, 0.01*peri, True)
+    warp = perspective.four_point_transform(image, approx.reshape(4, 2) * ratio)
 
-    # Find width and height of card's bounding rectangle
-    x,y,w,h = cv2.boundingRect(contour)
-    width, height = w, h
+    if (warp.shape[0] < warp.shape[1]):
+        warp = imutils.rotate_bound(warp, angle=90)
 
-    # Find center point of card by taking x and y average of the four corners.
-    average = np.sum(pts, axis=0)/len(pts)
-    cent_x = int(average[0][0])
-    cent_y = int(average[0][1])
-    center = [cent_x, cent_y]
-
-    # Warp card into 200x300 flattened image using perspective transform
-    warp = flattener(image, pts, w, h)
-
-    # Grab corner of warped card image and do a 4x zoom
-    #Qcorner = warp[0:CORNER_HEIGHT, 0:CORNER_WIDTH]
-    #Qcorner_zoom = cv2.resize(Qcorner, (0,0), fx=4, fy=4)
-    cv2.imshow("Output", warp)
+    warp = cv2.resize(warp, (450, 700)) # make it into an actual card shape
+    
+    # Get the top left corner of the card
+    corner = warp[0:165, 0:165]
+    corner_zoom = cv2.resize(corner, (0,0), fx=4, fy=4)
+    cv2.imshow("Output", corner_zoom)
     cv2.waitKey(0)
 
+    gray = cv2.cvtColor(corner_zoom, cv2.COLOR_BGR2GRAY)
 
-def flattener(image, pts, w, h):
-    """Flattens an image of a card into a top-down 200x300 perspective.
-    Returns the flattened, re-sized, grayed image.
-    See www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/"""
-    temp_rect = np.zeros((4,2), dtype = "float32")
-    
-    s = np.sum(pts, axis = 2)
+    img_w, img_h = np.shape(corner_zoom)[:2]
+    bkg_level = gray[int(img_h/100)][int(img_w/2)]
+    retval, thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)
+    dummy, cnts, hier = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    tl = pts[np.argmin(s)]
-    br = pts[np.argmax(s)]
+    cv2.imshow("Threshold", thresh)
+    cv2.waitKey(0)
 
-    diff = np.diff(pts, axis = -1)
-    tr = pts[np.argmin(diff)]
-    bl = pts[np.argmax(diff)]
+    # because the shapes might be slightly different, lets try and make a smaller section
+    # to focus on when reading the marking
 
-    # Need to create an array listing points in order of
-    # [top left, top right, bottom right, bottom left]
-    # before doing the perspective transform
+    for i in range(len(cnts)):
+        c = cnts[i]
 
-    if w <= 0.8*h: # If card is vertically oriented
-        print 'vertical'
-        temp_rect[0] = tl
-        temp_rect[1] = tr
-        temp_rect[2] = br
-        temp_rect[3] = bl
+        size = cv2.contourArea(c)
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.005*peri, True)
 
-    if w >= 1.2*h: # If card is horizontally oriented
-        print 'horizontal'
-        temp_rect[0] = bl
-        temp_rect[1] = tl
-        temp_rect[2] = tr
-        temp_rect[3] = br
+        if (size > 10000 and len(approx) > 4 and len(approx) < 8):
 
-    # If the card is 'diamond' oriented, a different algorithm
-    # has to be used to identify which point is top left, top right
-    # bottom left, and bottom right.
-    
-    print w
-    print 0.8*h
-    print 1.2*h
+            # We need three points to make a new rectangle.
+            # 1. middle point, lower and rightmost
+            # 2. Furthest point right, and lowest
+            # 3. Furthest point south, and right most
+            lm = [];
+            rm = [];
+            bm = [];
 
-    if w > 0.8*h and w < 1.2*h: #If card is diamond oriented
-        print 'diamond'
-        # If furthest left point is higher than furthest right point,
-        # card is tilted to the left.
-        if pts[1][0][1] <= pts[3][0][1]:
-            # If card is titled to the left, approxPolyDP returns points
-            # in this order: top right, top left, bottom left, bottom right
-            temp_rect[0] = pts[1][0] # Top left
-            temp_rect[1] = pts[0][0] # Top right
-            temp_rect[2] = pts[3][0] # Bottom right
-            temp_rect[3] = pts[2][0] # Bottom left
+            for i in range(len(approx)):
 
-        # If furthest left point is lower than furthest right point,
-        # card is tilted to the right
-        if pts[1][0][1] > pts[3][0][1]:
-            # If card is titled to the right, approxPolyDP returns points
-            # in this order: top left, bottom left, bottom right, top right
-            temp_rect[0] = pts[0][0] # Top left
-            temp_rect[1] = pts[3][0] # Top right
-            temp_rect[2] = pts[2][0] # Bottom right
-            temp_rect[3] = pts[1][0] # Bottom left
-        
-    maxWidth = 200
-    maxHeight = 300
+                pt = approx[i][0]
+                print pt
+                if pt[0] > 650:
+                    if len(bm) == 0:
+                        bm = pt
+                    elif len(bm) > 0 and bm[1] < pt[1]:
+                        bm = pt
+                    bm[0] = corner_zoom.shape[0]
 
-    # Create destination array, calculate perspective transform matrix,
-    # and warp card image
-    dst = np.array([[0,0],[maxWidth-1,0],[maxWidth-1,maxHeight-1],[0, maxHeight-1]], np.float32)
-    M = cv2.getPerspectiveTransform(temp_rect,dst)
-    warp = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
-    #warp = cv2.cvtColor(warp,cv2.COLOR_BGR2GRAY)
+                elif pt[1] > 650:
+                    print pt
+                    if len(rm) == 0:
+                        rm = pt
+                    elif len(rm) > 0 and rm[0] < pt[0]:
+                        rm = pt
+                    rm[1] = corner_zoom.shape[1]
 
-    return warp
+                else:
+                    if len(lm) == 0:
+                        lm = pt
+                    elif lm[0] < pt[0]:
+                        lm = pt
+                    elif lm[1] < pt[1]:
+                        lm = pt
+
+            
+            break;
+
+    cv2.circle(corner_zoom, tuple(lm), 5, (240, 0, 159), -1)
+    cv2.circle(corner_zoom, tuple(bm), 5, (240, 0, 159), -1)
+    cv2.circle(corner_zoom, tuple(rm), 5, (240, 0, 159), -1)
+
+    cv2.imshow("Image", corner_zoom)
+    cv2.waitKey(0)
+
+    coords = [tuple(lm), tuple(rm), tuple(bm), tuple([corner_zoom.shape[0], corner_zoom.shape[1]])];
+    coords = np.array(coords, dtype = "float32")
+    warp = perspective.four_point_transform(corner_zoom, coords)
+
+    # image = thresh[292:659, 262:659]
+    cv2.imshow("Image", warp)
+    cv2.waitKey(0)
+
 
 
 ##########################################################################################################################################
@@ -163,8 +139,10 @@ def flattener(image, pts, w, h):
 # load the input image and show its dimensions, keeping in mind that
 # images are represented as a multi-dimensional NumPy array with
 # shape no. rows (height) x no. columns (width) x no. channels (depth)
-image = cv2.imread("card4.jpg")
-image = imutils.resize(image, width=2000)
+original_image = cv2.imread("card2.jpg")
+ratio = original_image.shape[1] / float(2000)
+image = imutils.resize(original_image, width=2000)
+
 gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 blur = cv2.GaussianBlur(gray,(5,5),0)
 img_w, img_h = np.shape(image)[:2]
@@ -173,27 +151,12 @@ thresh_level = bkg_level + BKG_THRESH
 
 retval, thresh = cv2.threshold(blur,thresh_level,255,cv2.THRESH_BINARY)
 
-cnts_sort, cnt_is_card = find_cards(thresh)
+cards = find_cards(thresh)
 
-if len(cnts_sort) != 0:
-
-    # Initialize a new "cards" list to assign the card objects.
-    # k indexes the newly made array of cards.
-    cards = []
-    k = 0
-
+if len(cards) != 0:
     # For each contour detected:
-    for i in range(len(cnts_sort)):
-        cards.append(cnts_sort[i])
-
-    if (len(cards) != 0):
-        temp_cnts = []
-        for i in range(len(cards)):
-            if (cnt_is_card[i] == 1):
-                preprocess_card(cnts_sort[i], image)
-                temp_cnts.append(cards[i])
-        
-        cv2.drawContours(image, temp_cnts, -1, (255,0,0), 2)
+    for i in range(len(cards)):
+        preprocess_card(cards[i], original_image, ratio)
 
 
 #cv2.imshow("Output", image)
